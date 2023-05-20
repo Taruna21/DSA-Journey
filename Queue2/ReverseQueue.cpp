e50e6d499ffad36b8dd5ca10c5b89e6b6e17454c
Your Name <you@example.com>
2023-05-20 10:06:05 +0530

    -

---
+++ Queue2/ReverseQueue.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+//function Template for C++
+
+//Function to reverse the queue.
+queue<int> rev(queue<int> q)
+{
+    // add code here.
+    stack<int>s;
+    
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        
+        s.push(element);
+    }
+    
+    while(!s.empty()){
+        int element = s.top();
+        s.pop();
+        
+        q.push(element);
+    }
+    
+    return q;
+}
