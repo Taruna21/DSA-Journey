995484441895373478ba1ccf528793175e6038f4
Your Name <you@example.com>
2023-05-23 10:01:57 +0530

    implementtree

--- TREE1/implementTree.cpp
+++ TREE1/implementTree.cpp
@@ -1,21 +1,43 @@
 #include<iostream>
 using namespace std;
 
+class Node {
+    public:
+    int data;
+    Node * left;
+    Node * right;
+
+    Node(int data){
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+
+    }
+};
 
 
+Node * buildTree(){
 
+    int data;
+    cout << "Enter the data"<<endl;
+    cin >> data;
 
-
-
-Node * buildTree(int data){
     if(data ==-1){
         return NULL;
     }
 
     Node* root = new Node(data);
+    root -> left = buildTree();
+    root-> right = buildTree();
+
+    return root;
 }
 
 int main(){
-    Node* root;
+    Node* root = NULL;
+    root = buildTree();
+    cout
+    return 0;
+
 
 }
